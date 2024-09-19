2e5b9e60ab073872c26895bce46dd113d91823ac
aaron silverstein <aaronsilverstein@aarons-MacBook-Pro.local>
2024-09-19 17:30:25 -0400

    created input component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import TransactionDetails from './components/TransactionDetails';
+import TransactionSearch from './components/TransactionSearch/TransactionSearch';
+import TransactionStatus from './components/TransactionStatus';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="appContainer">
+      <TransactionSearch/>
+    </section>
   );
 }
 
---
+++ src/components/TransactionSearch/TransactionSearch.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './styles.css';
+const TransactionSearch  = () => {
+    return (
+        <section className="transactionSearchContainer">
+            <div className="transactionSearchInnerContainer">
+                <label className="transactionSearchLabel" for="Transaction ID">Transaction ID</label>
+                <input className="transactionSearchInput" type="text" class="transactionInput"></input>
+            </div>
+            <button className="searchButton">Search</button>
+        </section>
+    )
+}
+
+export default TransactionSearch;
